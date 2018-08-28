efa4a944a009efac8c39c00e6bc1f95f985c5bab
Biswa Nath Ghosh <tapos.aa@gmail.com>
2018-08-28 13:06:44 +0600

    oauth2 full implemented

---
+++ WebApiOauth2/Controllers/WebApiController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+
+namespace WebApiOauth2.Controllers
+{
+    [Authorize]
+    public class WebApiController : ApiController
+    {
+        // GET api/WebApi
+        public IEnumerable<string> Get()
+        {
+            var info = RequestContext.Principal.Identity;
+            return new string[] { "Hello REST API", "I am Authorized", "hjh" };
+        }
+
+        // GET api/WebApi/5
+        public string Get(int id)
+        {
+            return "Hello Authorized API with ID = " + id;
+        }
+
+        // POST api/WebApi
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/WebApi/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/WebApi/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
